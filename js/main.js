22d5f6534c43a371bec757133ecd3921f9c92868
irinaponomarenko <ponomarenko02041993@gmail.com>
2021-05-06 14:38:42 +0300

    media

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+    //--------------------------dropdown faq----------------------//
 
+    $(".item-faq").click(function() {
+        $(".item-faq").children(".container-info-faq").slideUp(300);
+        $(".close-faq-item").removeClass('active-btn-faq');
+        if ($(this).children(".container-info-faq").is(":hidden")) {
+            $(this).children(".container-info-faq").slideDown(300);
+            $(this).find('.close-faq-item').addClass('active-btn-faq');
+        };
+    });
+
+
+    //--------------------------open window faq----------------------//
+
+    $(".faq-btn").click(function (){
+       $(".wrapper-dark").addClass("active-bg");
+       $(".container-faq").addClass("active-faq");
+    });
+
+    $(".close-window").click(function (){
+        $(this).closest(".wrapper-dark").removeClass("active-bg");
+        $(this).closest(".wrapper-dark").find(".container-faq").removeClass("active-faq");
+        $(this).closest(".wrapper-dark").find(".container-connect").removeClass("active-connect");
+    });
+
+    $(".connect").click(function (){
+        $(".wrapper-dark").addClass("active-bg");
+        $(".container-connect").addClass("active-connect");
+    });
 
 });
